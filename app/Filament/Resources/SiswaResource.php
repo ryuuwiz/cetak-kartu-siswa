395b0d22776593a93500c10238e000574dc8d8e2
Ryu K <ryuuwiz@gmail.com>
2024-12-04 19:42:50 +0700

    refactor: siswa resource

--- app/Filament/Resources/SiswaResource.php
+++ app/Filament/Resources/SiswaResource.php
@@ -112,9 +112,9 @@ class SiswaResource extends Resource
                 Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
-//                Tables\Actions\BulkActionGroup::make([
-//                    Tables\Actions\DeleteBulkAction::make(),
-//                ]),
+                //                Tables\Actions\BulkActionGroup::make([
+                //                    Tables\Actions\DeleteBulkAction::make(),
+                //                ]),
             ]);
     }
 
